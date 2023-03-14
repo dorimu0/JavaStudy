34107e032a463e8d0d601a150d91292fafd2fb03
dorimu0 <121004915+dorimu0@users.noreply.github.com>
2023-03-14 11:32:06 +0900

    Update StringTest.java

--- StringTest.java
+++ StringTest.java
@@ -21,6 +21,22 @@ public class StringTest {
     System.out.println("결과값은 " + x);
     String s = "1234";
     Integer.parseInt(s);
+    
+    // 예제
+//     String str;
+// 		Scanner scan = new Scanner(System.in);
+		
+// 		while (true) {
+// 			System.out.println("문자열을 입력하세요 > ");
+// 			str = scan.next();
+// 			if ( str.equals("quite") == true) 
+// 				break;
+// 			if (str.matches("^www\\.(.+)")) {
+// 			System.out.println(str + " 은 'www'로 시작합니다.");
+// 			} else {
+// 				System.out.println(str + " 은 'www'로 시작하지 않습니다.");
+// 			}
+// 		}
 
   }
   
