951696ef88f22903ac3f5b532aae80da2032c1f5
dorimu0 <121004915+dorimu0@users.noreply.github.com>
2023-03-17 17:49:17 +0900

    Add files via upload

---
+++ ArrayProcTest.java
@@ -0,0 +1,13 @@
+package class1;
+
+public class ArrayProcTest {
+  final static int STUDENTS = 5;
+
+  public static void main(String args[]){
+    int[] scores = new int[STUDENTS];
+    ArrayProc obj = new ArrayProc();
+    obj.getValues(scores);
+    System.out.println("평균은 = " + obj.getAverage(scores));
+  }
+  
+}
