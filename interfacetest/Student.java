02336f24355379bd07ecb4bca565782d9cecdcf1
dorimu0 <dbsakgk45@g.yju.ac.kr>
2023-04-20 21:23:05 +0900

    20230420

---
+++ interfacetest/Student.java
@@ -0,0 +1,21 @@
+package interfacetest;
+
+public interface Student implements {
+  private String name;
+  private String dept;
+  
+  @Override
+  public int compareTo(Comparable obj) {
+    // null인 경우는 없다고 가정
+    // interface는 class처럼 데이터 타입으로 사용 가능
+    // 특정 interface 타입의 변수는 그 interface를 구현하는
+    // 모든 클래스의 객체를 담을 수 있다.
+    Student std = (Student)obj;
+    // String 클래스는 java.lang 패키지의 Comparable 인터페이스를
+    // 이미 구현하고 있다. 즉, 그 인터페이스의 추상 메소드 인
+    // compareTo 메소드를 구현
+    // String의 compareTo 메소드는 사전 순서에 따라 비교 대상보다
+    // 사전 순서상 앞에 위치하면 -1, 같으면 0, 뒤에 위치하면 1을 반환
+    return name.compareTo(std.getName());
+  }
+ }
---
+++ interfacetest/Test.java
@@ -0,0 +1,24 @@
+package interfacetest;
+
+public class Test {
+  public static void main(String[] args) {
+    String s1 = "ABC";
+    String s2 = "ABD";
+    System.out.println(s1.compareTo(s2));
+    Student std1 = new Student("일지매", "컴정");
+    Student std2 = new Student("홍길동", "컴정");
+          
+  }
+
+
+  public static void findLarger(Comparable c1, Comparable c2){
+    if (c1.compareTo(c2) == 0){
+      System.out.println("같다");
+    } else if(c1.compareTo(c2) > 0){
+      System.out.println("첫번째 큼");
+    } else {
+      System.out.println("두번째 큼");
+    }
+  }
+  
+}
